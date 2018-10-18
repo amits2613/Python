d417350723804de899665211fe50dd48606a2a56
Amit Sharma <amits2613@gmail.com>
2018-10-18 11:54:13 +0530

    Update web_login_testing.py
    updated variable

--- web_login_testing.py
+++ web_login_testing.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common import action_chains
+import time
+import os
 
-br = webdriver.Firefox()
-br.get('https://www.facebook.com/')
-email = br.find_element_by_id('email')
+driver self = webdriver.Firefox()
+self.get('https://www.facebook.com/')
+email = self.find_element_by_id('email')
 email.send_keys('ixxxxxxxx@gmail.com')
-pas = br.find_element_by_id('pass')
+pas = self.find_element_by_id('pass')
 pas.send_keys('xxxxxxxxxx')
 pas.submit()
